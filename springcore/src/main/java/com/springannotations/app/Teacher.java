c7095e3507c49e14c8dc06f1cb03018470f24652
THAMIZH221909 <dthamizhselvi2004@gmail.com>
2025-09-01 20:06:37 +0530

    Commit

---
+++ springcore/src/main/java/com/springannotations/app/Teacher.java
@@ -0,0 +1,7 @@
+//Program to implement Dependency Injection using setter through annotations
+package com.springannotations.app;
+
+public interface Teacher {
+	public String getTeacherInfo();
+	
+}
